a208df3e9e111b9aee84c535ee6c14c7429f472a
thx-to <tlsekghk@gmail.com>
2024-10-25 14:20:24 +0900

    Local Updated

---
+++ kh_1TP_Java/src/TEST_241025_취합_수정/Customer/SetMenu.java
@@ -0,0 +1,61 @@
+package TEST_241025_취합_수정.Customer;
+
+public class SetMenu {
+    SingleMenu burger;
+    SingleMenu side;
+    SingleMenu drink;
+    int price;
+    int count;
+
+    public SetMenu(SingleMenu burger, SingleMenu side, SingleMenu drink, int count) {
+        this.burger = burger;
+        this.side = side;
+        this.drink = drink;
+        this.count = count;
+        this.price = (getBurger().price + getSide().price + getDrink().price) - 1200;
+    }
+
+    public void changeCount(int cnt) {
+        this.count += cnt;
+    }
+
+    public void setBurger(SingleMenu burger) {
+        this.burger = burger;
+    }
+
+    public void setSide(SingleMenu side) {
+        this.side = side;
+    }
+
+    public void setDrink(SingleMenu drink) {
+        this.drink = drink;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public SingleMenu getBurger() {
+        return burger;
+    }
+
+    public SingleMenu getSide() {
+        return side;
+    }
+
+    public SingleMenu getDrink() {
+        return drink;
+    }
+}
---
+++ kh_1TP_Java/src/TEST_241025_취합_수정/VO/Order_RecordVO.java
@@ -0,0 +1,77 @@
+package TEST_241025_취합_수정.VO;
+
+import java.sql.Timestamp;
+
+public class Order_RecordVO {
+    private String orderCode;
+    private String orderList;
+    private Timestamp orderTime; // LocalDateTime 을 대신 써야할지 아직 고민중.
+    private int orderPrice;
+    private String userId;
+    private String storeId;
+
+    public Order_RecordVO(String orderCode, String orderList, Timestamp orderTime, int orderPrice, String userId, String storeId) {
+        this.orderCode = orderCode;
+        this.orderList = orderList;
+        this.orderTime = orderTime;
+        this.orderPrice = orderPrice;
+        this.userId = userId;
+        this.storeId = storeId;
+    }
+
+    public Order_RecordVO(String userId, String storeId, String orderList, int orderPrice) {
+    }
+
+    public String getOrderCode() {
+        return orderCode;
+    }
+
+    public String getOrderList() {
+        return orderList;
+    }
+
+    public Timestamp getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Timestamp orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getStoreId() {
+        return storeId;
+    }
+
+    public void setOrderCode(String orderCode) {
+        this.orderCode = orderCode;
+    }
+
+    public void setOrderList(String orderList) {
+        this.orderList = orderList;
+    }
+
+
+
+    public void setOrderPrice(int orderPrice) {
+        if (orderPrice < 0) {   // null은 허용 안하니까 조건은 음수일 수 없다로만 설정.
+            throw new IllegalArgumentException("제품 가격은 음수일 수 없습니다.");
+        }
+        this.orderPrice = orderPrice;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setStoreId(String storeId) {
+        this.storeId = storeId;
+    }
+}
