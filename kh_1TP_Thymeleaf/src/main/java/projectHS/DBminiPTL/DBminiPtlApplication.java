f3bd4e79609e112940a003c731bfd017189fd3f4
thx-to <tlsekghk@gmail.com>
2024-10-28 10:35:28 +0900

    Local Updated

---
+++ kh_1TP_Thymeleaf/src/main/java/projectHS/DBminiPTL/DBminiPtlApplication.java
@@ -0,0 +1,13 @@
+package projectHS.DBminiPTL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBminiPtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBminiPtlApplication.class, args);
+	}
+
+}
---
+++ kh_1TP_Thymeleaf/src/test/java/projectHS/DBminiPTL/DBminiPtlApplicationTests.java
@@ -0,0 +1,13 @@
+package projectHS.DBminiPTL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBminiPtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
